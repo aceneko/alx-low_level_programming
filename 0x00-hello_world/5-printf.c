544810b9a54c4b5ad662aaf0100240450a4d4982
aceneko <fotaru11@gmail.com>
2023-03-16 19:13:18 -0700

    trailing whitespace

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is the beginning of program execution
+ * main - this is the beginning of execution
  *
- * Return: 0 is when successful 
+ * Return: successful end of execution is 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
