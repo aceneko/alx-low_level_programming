0b57961e4ce0dc14b70ed5c1ecc574e0bec7b160
aceneko <fotaru11@gmail.com>
2023-03-16 20:23:32 -0700

    doneig

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - beginning
  *
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
